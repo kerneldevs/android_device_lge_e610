8dc11f02f9b332d3c854e7840de4402d3209de4c
RonGokhale <cips173@gmail.com>
2013-08-23 02:34:48 +0530

    e610: updates for evervolv
    Change-Id: I367641af092a0d01eddb62b333b66cbba3b11d0d

--- ev.mk
+++ ev.mk
@@ -1,19 +1,15 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/ev/config/gsm.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common evervolv stuff.
+$(call inherit-product, vendor/ev/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/e610/e610.mk)
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 480
-TARGET_SCREEN_WIDTH := 320
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := e610
-PRODUCT_NAME := cm_e610
+PRODUCT_NAME := ev_e610
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-E610
 PRODUCT_MANUFACTURER := LGE
@@ -28,3 +24,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Release name and versioning
 PRODUCT_RELEASE_NAME := OptimusL5
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
+
+PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your LG Optimus L5 \n------------------------------------------------\n"
+
+# Copy compatible bootanimation
+PRODUCT_COPY_FILES +=  \
+    vendor/ev/prebuilt/hvga/media/bootanimation.zip:system/media/bootanimation.zip
+
+# Hot reboot
+PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/hot_reboot
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo cm_e610-eng
-add_lunch_combo cm_e610-userdebug
+add_lunch_combo ev_e610-eng
+add_lunch_combo ev_e610-userdebug
